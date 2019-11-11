694ef94939943cd7b4bb4fee4574b4c7032e6836
maciejkorolik <maciej.korolik@gmail.com>
2019-11-11 18:47:42 +0100

    Updates

---
+++ precache-manifest.e22732f4dd5104eb06c2257da782510d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad2c4420c79e6cc4df9ab795f93d0e0",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "d2856f8b2b486262d594",
+    "url": "/react-memory-game/static/css/2.a071d2a7.chunk.css"
+  },
+  {
+    "revision": "d2856f8b2b486262d594",
+    "url": "/react-memory-game/static/js/2.a2dbf90f.chunk.js"
+  },
+  {
+    "revision": "3dbc58c537af386ccb11",
+    "url": "/react-memory-game/static/js/main.c1294294.chunk.js"
+  },
+  {
+    "revision": "0e683e2b4c4b17d064e0",
+    "url": "/react-memory-game/static/js/runtime~main.75206554.js"
+  },
+  {
+    "revision": "58fe533cec6e86fbb882bf8b0811c1b9",
+    "url": "/react-memory-game/static/media/01.58fe533c.png"
+  },
+  {
+    "revision": "e50aa4b46f318ae5e9e62ec5d8a6ddb2",
+    "url": "/react-memory-game/static/media/02.e50aa4b4.png"
+  },
+  {
+    "revision": "25dc39b2fc5db3c171ab7e2fce877542",
+    "url": "/react-memory-game/static/media/03.25dc39b2.png"
+  },
+  {
+    "revision": "250257eeef0afc42fa87708a78feb1ed",
+    "url": "/react-memory-game/static/media/04.250257ee.png"
+  },
+  {
+    "revision": "e6d21f86a97e1ed6665169811dc37407",
+    "url": "/react-memory-game/static/media/05.e6d21f86.png"
+  },
+  {
+    "revision": "a647bf6def1377c6e21bf9d43987ee0a",
+    "url": "/react-memory-game/static/media/06.a647bf6d.png"
+  },
+  {
+    "revision": "65fe91d4b15c7c41c6de793bf6d051ac",
+    "url": "/react-memory-game/static/media/07.65fe91d4.png"
+  },
+  {
+    "revision": "61528c3ba74a04d86642b43f5d85acbc",
+    "url": "/react-memory-game/static/media/08.61528c3b.png"
+  },
+  {
+    "revision": "00cdf2cf6d67744dfdbf080e78078e76",
+    "url": "/react-memory-game/static/media/09.00cdf2cf.png"
+  },
+  {
+    "revision": "804e827a6273b51d311c923d9cdbb712",
+    "url": "/react-memory-game/static/media/Finish.804e827a.png"
+  },
+  {
+    "revision": "3b0759d4919eb78372ac6e52a1189f3d",
+    "url": "/react-memory-game/static/media/Welcome.3b0759d4.png"
+  },
+  {
+    "revision": "b95219bec3df8ae8bc3747069eca1b6d",
+    "url": "/react-memory-game/static/media/mclaren-latin-400.b95219be.woff2"
+  },
+  {
+    "revision": "dbcb597e06df2dae0f47d943bfe51df8",
+    "url": "/react-memory-game/static/media/mclaren-latin-400.dbcb597e.woff"
+  }
+]);
